052d4542e214b74bade419f2a77a60601313744e
Colin Poon <me@colinpoon.com>
2021-11-10 13:40:07 -0800

    find_by/update

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -3,5 +3,10 @@ require_relative './exercise_1'
 
 puts "Exercise 2"
 puts "----------"
+# use .find_by to search
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
 
-# Your code goes here ...
+# use update to change info 
+
+@store1.update(name: "Granville Island")
