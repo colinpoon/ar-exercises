7ff67dcc0faba8ef5f22d7481ef64b91b666d255
Colin Poon <me@colinpoon.com>
2021-11-10 14:00:02 -0800

    ex 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts @add = Store.sum(:annual_revenue)
+# 4114000
+puts @avg = Store.average(:annual_revenue)
+# 822800.0
+
+puts @mil = Store.where("annual_revenue > ?", 1000000).count
+# 2
