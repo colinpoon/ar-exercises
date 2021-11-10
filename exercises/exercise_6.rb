47fcbce9379119ed2595f316ec012add37f9e669
Colin Poon <me@colinpoon.com>
2021-11-10 14:08:50 -0800

    ex6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,26 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+#@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Colin", last_name: "Poon", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Ha", last_name: "Cindy", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Kiki", last_name: "Sea", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Moon", last_name: "Pie", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Persons", last_name: "Name", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Zoom", last_name: "Zoom", hourly_rate: 10)
+
+@store1.employees.create(first_name: "La", last_name: "La", hourly_rate: 10)
+
+@store1.employees.create(first_name: "Ki", last_name: "Ki", hourly_rate: 10)
+
+@store2.employees.create(first_name: "Tom", last_name: "Ass", hourly_rate: 10)
+
+@store2.employees.create(first_name: "Another", last_name: "Person", hourly_rate: 10)
